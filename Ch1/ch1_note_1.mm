f227adedc34e0eaa992f6a901632696f24803064
Summerlve <dzxx0563zh@126.com>
2019-11-25 22:35:53 +0800

    CO1_A

---
+++ Ch1/ch1_note_1.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1574591989165" ID="ID_698895901" MODIFIED="1574592637768" TEXT="Network Core">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1574592060057" HGAP="11" ID="ID_374701083" MODIFIED="1574592721429" POSITION="right" TEXT="Circuit Switching (non-shared)" VSHIFT="-89">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1574592374039" HGAP="36" ID="ID_761891297" MODIFIED="1574592437037" VSHIFT="-48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="FDMA.png" />
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1574592408316" HGAP="47" ID="ID_1556779393" MODIFIED="1574592423235" VSHIFT="18">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="TDMA.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1574592618389" HGAP="50" ID="ID_342525054" MODIFIED="1574592717772" POSITION="right" TEXT="Packet Switching (shared)" VSHIFT="-45">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1574592750348" HGAP="29" ID="ID_399778425" MODIFIED="1574592796137" TEXT="Store (queue)" VSHIFT="-21">
+<font NAME="SansSerif" SIZE="27"/>
+</node>
+<node CREATED="1574592767727" HGAP="29" ID="ID_634483196" MODIFIED="1574592791985" TEXT="Forward" VSHIFT="38">
+<font NAME="SansSerif" SIZE="27"/>
+</node>
+</node>
+</node>
+</map>
