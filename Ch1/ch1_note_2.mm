d92cdccac7312af3001d0d78c57e721efe334248
Summerlve <dzxx0563zh@126.com>
2019-12-01 20:38:09 +0800

    C01_B

---
+++ Ch1/ch1_note_2.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1575167690131" ID="ID_241610394" MODIFIED="1575168630021" TEXT="Network Access (&#x7f51;&#x7edc;&#x63a5;&#x5165;) &amp; Physical media (&#x7269;&#x7406;&#x5a92;&#x4ecb;)">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1575168188188" HGAP="10" ID="ID_1177679974" MODIFIED="1575168630020" POSITION="right" TEXT="Network Access (&#x7f51;&#x7edc;&#x63a5;&#x5165;)" VSHIFT="-20">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1575167801133" HGAP="24" ID="ID_1639888307" MODIFIED="1575168647055" TEXT="Dialup via modem (&#x62e8;&#x53f7;&#x4e0a;&#x7f51;)" VSHIFT="-18">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575167810711" HGAP="21" ID="ID_1753370352" MODIFIED="1575168630020" TEXT="ADSL (FDM)" VSHIFT="-18">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575167826517" ID="ID_1800961880" MODIFIED="1575168630018" TEXT="HFC (Hybrid fiber coaxial cable &lt;&#x6df7;&#x5408;&#x5149;&#x7ea4;&#x540c;&#x8f74;&#x7535;&#x7f06;(&#x4e3b;&#x5e72;&#x7f51;&#x5149;&#x7ea4;&#xff0c;&#x540c;&#x8f74;&#x7535;&#x7f06;&#x5230;&#x6237;)&gt;)">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575168028761" HGAP="24" ID="ID_915411659" MODIFIED="1575168630018" TEXT="LAN (local area network)" VSHIFT="15">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575168033422" HGAP="28" ID="ID_439894347" MODIFIED="1575168630017" TEXT="WAN (wide area network)" VSHIFT="20">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+</node>
+<node CREATED="1575168369020" HGAP="14" ID="ID_1593967085" MODIFIED="1575168630017" POSITION="right" TEXT="Physical media (&#x7269;&#x7406;&#x5a92;&#x4ecb;)" VSHIFT="15">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1575168424397" HGAP="23" ID="ID_114617676" MODIFIED="1575168630017" TEXT="Twisted Pair (TP) (&#x53cc;&#x7ede;&#x7ebf;)" VSHIFT="-15">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575168443812" HGAP="26" ID="ID_1171744173" MODIFIED="1575168630016" TEXT="Coaxial Cable (&#x540c;&#x8f74;&#x7535;&#x7f06;)" VSHIFT="-7">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575168489854" HGAP="24" ID="ID_1503561031" MODIFIED="1575168630016" TEXT="Fiber optic cable (&#x5149;&#x7ea4;)" VSHIFT="10">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+<node CREATED="1575168533618" HGAP="25" ID="ID_1811612755" MODIFIED="1575168630015" TEXT="Radio (&#x65e0;&#x7ebf;&#x7535;)" VSHIFT="13">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+</node>
+</node>
+</map>
