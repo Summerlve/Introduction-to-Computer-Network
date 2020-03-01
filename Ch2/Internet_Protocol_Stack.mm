61af9c9255a5dd273d45f207b03d6dc19a424a54
Summerlve <dzxx0563zh@126.com>
2020-03-01 22:17:53 +0800

    restart learing

---
+++ Ch2/Internet_Protocol_Stack.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1578144056859" ID="ID_581866757" MODIFIED="1578195206853" TEXT="Internet Protocol Stack">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1578144127407" HGAP="36" ID="ID_99493990" MODIFIED="1578194924764" POSITION="right" TEXT="Layer 5: Application Layer(message)" VSHIFT="-32">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1578144297117" ID="ID_1092904099" MODIFIED="1578195441651" TEXT="C/S (client/server)" VSHIFT="-15">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1578144315519" ID="ID_297703215" MODIFIED="1578195443372" TEXT="P2P(peer to peer)" VSHIFT="-7">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1578144332465" HGAP="21" ID="ID_646197468" MODIFIED="1578195447680" TEXT="Hybrid of C/S and P2P" VSHIFT="8">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1578195399184" HGAP="21" ID="ID_1143661936" MODIFIED="1578195460389" TEXT="Processes communicating across network(socket)" VSHIFT="12">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1578144156916" HGAP="35" ID="ID_281559503" MODIFIED="1578195298740" POSITION="right" TEXT="Layer 4: Transport Layer(segment)" VSHIFT="-45">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1578195190272" ID="ID_929301248" MODIFIED="1578195314273" TEXT="TCP" VSHIFT="-12">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1578195214604" HGAP="18" ID="ID_1697694571" MODIFIED="1578195316489" TEXT="UDP" VSHIFT="16">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1578144153679" HGAP="36" ID="ID_425846185" MODIFIED="1578194924764" POSITION="right" TEXT="Layer 3: Network Layer(datagram)" VSHIFT="-9">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1578195021763" HGAP="23" ID="ID_256237116" MODIFIED="1578195048314" TEXT="Router" VSHIFT="-16">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1578144232712" HGAP="37" ID="ID_1219993233" MODIFIED="1578194924764" POSITION="right" TEXT="Layer 2: Link Layer(frame)" VSHIFT="26">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1578195028680" HGAP="36" ID="ID_566479366" MODIFIED="1578195042835" TEXT="Switch" VSHIFT="-25">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1578144249227" HGAP="38" ID="ID_70102401" MODIFIED="1578194924764" POSITION="right" TEXT="Layer 1: Physical Layer(bit)" VSHIFT="36">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
